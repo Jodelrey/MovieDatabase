2f39275ec94511318a3c1ac9392cb963c2f523ae
Lujan Sanchez <mlulasan@gmail.com>
2020-08-05 19:06:00 -0300

    subo archivos iniciales

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Karla', Verdana, Geneva, Tahoma, sans-serif;
+}
+
+`;
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle />
     </div>
   );
 }
